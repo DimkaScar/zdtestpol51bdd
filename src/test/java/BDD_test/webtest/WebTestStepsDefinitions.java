3691e99b08bb281eaeee925c40c760190b17ab0a
DimkaScar <push.dmytro@gmail.com>
2021-03-06 14:14:22 +0100

    compelted test wiith first blog

--- src/test/java/BDD_test/webtest/WebTestStepsDefinitions.java
+++ src/test/java/BDD_test/webtest/WebTestStepsDefinitions.java
@@ -4,18 +4,26 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebTestStepsDefinitions{
 
     WebDriver driver;
+    WebDriverWait wait;
+    String firstBlogTitle;
 
     @Before
     public void setup(){
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
     }
 
     @Given("I go to devTo main page")
@@ -24,11 +32,19 @@ public class WebTestStepsDefinitions{
     }
     @When("I click on first blog displayed")
     public void i_click_on_first_blog_displayed() {
-        driver.findElement(By.className("navigation-progress"));
+       //driver.findElement(By.className("crayons-story__body")).click();
+        WebElement firstBlog = driver.findElement(By.cssSelector("h2.crayons-story__title > a"));
+        firstBlogTitle = firstBlog.getText();
+        firstBlog.click();
     }
+
     @Then("I should be redirected to blog page")
     public void i_should_be_redirected_to_blog_page() {
-
+        wait.until(ExpectedConditions.titleContains(firstBlogTitle));
+        WebElement blogTitle = driver.findElement(By.tagName("h1"));
+        String blogTitleText = blogTitle.getText();
+        System.out.println("to jest tex mojego bloga : " + blogTitleText);
+        Assert.assertEquals(firstBlogTitle, blogTitleText);
     }
 
 }
